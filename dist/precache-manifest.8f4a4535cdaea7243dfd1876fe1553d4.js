7b1f214a32a772084ba581d3e25be4d99f064f98
majed <majed@prine.ch>
2018-06-08 20:15:35 +0200

    built the dist

---
+++ dist/precache-manifest.8f4a4535cdaea7243dfd1876fe1553d4.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "71fd5a84964b48e1923794c41ce25c36",
+    "url": "/img/courses/vue1.png"
+  },
+  {
+    "revision": "68b329da9893e34099c7d8ad5cb9c940",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "1f8b899c242e01f225a8919e93361f7f",
+    "url": "/img/courses/vue2.jpeg"
+  },
+  {
+    "revision": "44d24a9209f37f179d1a",
+    "url": "/css/chunk-vendors.3dda5bf8.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "70df66e9356072ce3d1ddf27a3d61ce9",
+    "url": "/img/epicschool_logo_2.70df66e9.png"
+  },
+  {
+    "revision": "d14238a85f0ea7a31e6054216dc28543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a48690078e9f17dd2e42aba93051d0b5",
+    "url": "/css/landing.css"
+  },
+  {
+    "revision": "9e725b0548fb9cbd569b4eed85a7e495",
+    "url": "/img/courses/code.jpg"
+  },
+  {
+    "revision": "1f08c9c67bef956926bae15793926ea7",
+    "url": "/img/courses/csharp.jpg"
+  },
+  {
+    "revision": "0514d435032aec9bcd499e1e1220897e",
+    "url": "/img/courses/3d.png"
+  },
+  {
+    "revision": "1782af90c04471ff7aefd1752fc6aed7",
+    "url": "/img/courses/gamemaker.jpg"
+  },
+  {
+    "revision": "a28d60993fcd43aa707d701b2c149731",
+    "url": "/img/courses/helloworld.jpg"
+  },
+  {
+    "revision": "2f9cf63aadfd79c1aa880aaf697064dd",
+    "url": "/img/courses/laravel.jpg"
+  },
+  {
+    "revision": "b3a406974b3dfd9826a2dbc756d515a1",
+    "url": "/img/courses/php.png"
+  },
+  {
+    "revision": "b13564d8fb71529f47e3cfb58d029b4b",
+    "url": "/img/courses/programming1.jpg"
+  },
+  {
+    "revision": "44d24a9209f37f179d1a",
+    "url": "/js/chunk-vendors.44d24a92.js"
+  },
+  {
+    "revision": "81df40b0c79384fd2ba4",
+    "url": "/js/app.81df40b0.js"
+  },
+  {
+    "revision": "49545e3493e574d4dc825dcfbbf329c8",
+    "url": "/img/courses/vuewithlaravel.png"
+  },
+  {
+    "revision": "002474823d306f1f7557fc6051189f5a",
+    "url": "/img/courses/vue3.png"
+  },
+  {
+    "revision": "883b3b5a905bd87eb72117f35a915dec",
+    "url": "/img/courses/webdesign.jpg"
+  },
+  {
+    "revision": "468dcce53f4075162557cf79f3771506",
+    "url": "/img/gifs/eye.gif"
+  },
+  {
+    "revision": "073cf0e58f1a5b8f8b1bded8846bc1c0",
+    "url": "/img/gifs/codegeass.gif"
+  },
+  {
+    "revision": "86e29656a8fef5101a3fb705346feb4f",
+    "url": "/img/github.jpg"
+  },
+  {
+    "revision": "00959572c5b86749b3dc4f91dba9d176",
+    "url": "/img/gifs/Sharingan.gif"
+  },
+  {
+    "revision": "09cfe3cd7b8cf6f149039cc23ebd2dcf",
+    "url": "/img/gifs/Satura_power.gif"
+  },
+  {
+    "revision": "c2051540b84f49aec046201d8f4e1812",
+    "url": "/img/letsTalk.jpg"
+  },
+  {
+    "revision": "d4d16eeac9587d2da20c386cfd7b575e",
+    "url": "/img/loaders/ripple.gif"
+  },
+  {
+    "revision": "c42942a4b7349a1f9bcc0c875edd2665",
+    "url": "/img/loaders/spinner.gif"
+  },
+  {
+    "revision": "ff18ba19e791abf2576f13e74e807932",
+    "url": "/img/sci_fi_classroom.jpg"
+  },
+  {
+    "revision": "508ff804229c6ce3ce1b8f39a886eea5",
+    "url": "/img/sci_fi_classroom_2.jpg"
+  },
+  {
+    "revision": "636e410b58ce11c916dd50d6b2f4a044",
+    "url": "/img/sci_fi_classroom.png"
+  },
+  {
+    "revision": "06723ab91873b2b90eec6f67b9375ef0",
+    "url": "/img/team/majed.png"
+  },
+  {
+    "revision": "a69a8fb5782d83c87d640525968ff373",
+    "url": "/img/sci-fi-castle-in-the-light.jpg"
+  },
+  {
+    "revision": "bc66163ea1e6e40a83770a57e56eb4af",
+    "url": "/img/team/mark.png"
+  },
+  {
+    "revision": "9fd46b98f417bb0ffb9c052e70cead14",
+    "url": "/img/gifs/flame.gif"
+  },
+  {
+    "revision": "81df40b0c79384fd2ba4",
+    "url": "/css/app.b385916b.css"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b7952d7905ab731457fd6ebf5a6ac985.js"
+  "/precache-manifest.8f4a4535cdaea7243dfd1876fe1553d4.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
