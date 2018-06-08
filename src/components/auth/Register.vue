d99f52938ef06f7c3e7afc5c0725e38ee231a9a5
majed <majed@prine.ch>
2018-06-08 16:10:38 +0200

    working on login and register pages

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -1,73 +1,80 @@
 <template>
 <div class="columns is-mobile">
-    <div class="column is-6-desktop is-offset-3-desktop is-8-tablet is-offset-2-tablet is-10-mobile is-offset-1-mobile">
-        <br/>
-        <h2 class="subtitle">Registrieren</h2>
-        <div class="field">
-            <p class="control has-icons-left has-icons-right">
-                <input required v-model.lazy="user.firstname"  id="login_firstname" name="firstname" class="input" type="text" placeholder="Vorname">
-                <span class="icon is-small is-left">
-                  <i class="fa fa-user"></i>
-                </span>
-                 <span v-if="errorMsg=='firstname'" class="help is-danger">Bitte geben Sie Ihren Namen ein</span>      
-            </p>
-        </div>
 
-        <div class="field">
-            <p class="control has-icons-left has-icons-right">
-                <input required v-model.lazy="user.lastname"  id="login_lastname" name="lastname" class="input" type="text" placeholder="Nachname">
-                <span class="icon is-small is-left">
-                  <i class="fa fa-user"></i>
-                </span>
-                 <span v-if="errorMsg=='lastname'" class="help is-danger">Bitte geben Sie Ihren Namen ein</span>      
-            </p>
-        </div>
+        <div class="column is-4-desktop is-offset-4-desktop is-8-tablet is-offset-2-tablet is-12-mobile ">
+          <h3 class="title has-text-grey">Sign up</h3>
+          <p class="subtitle has-text-grey">Please Sign up to unlock teh 1337 Power</p>
+          <div class="box">
+            <figure class="avatar">
+              <img class="login-image" src="img/gifs/Satura_power.gif">
+              
+            </figure>
+            <form>
+                <div class="field">
+                    <p class="control has-icons-left has-icons-right">
+                        <input required v-model.lazy="user.firstname"  id="login_firstname" name="firstname" class="input" type="text" placeholder="First name">
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-user"></i>
+                        </span>
+                        <span v-if="errorMsg=='firstname'" class="help is-danger">Please enter your first name</span>      
+                    </p>
+                </div>
 
-        <div class="field">
-            <p class="control has-icons-left has-icons-right">
-                <input required v-model.lazy="user.email" id="login_email" name="email" class="input" type="email" placeholder="E-Mail">
-                <span class="icon is-small is-left">
-                  <i class="fa fa-envelope"></i>
-                </span>
-                <span v-if="errorMsg=='email'" class="help is-danger">Bitte geben Sie eine gültige E-Mail-Adresse ein</span>    
-                <span v-if="errorMsg=='emailTaken'" class="help is-danger">Diese E-Mail-Adresse wird schon verwendet</span>
-            </p>
-        </div>
-        <div class="field">
-            <p class="control has-icons-left">
-                <input required v-model.lazy="user.password" id="password" class="input" type="password" placeholder="Passwort">
-                <span class="icon is-small is-left">
-                  <i class="fa fa-lock"></i>
-                </span>
-                <span v-if="errorMsg=='password'" class="help is-danger">Bitte Passwort eingeben</span> 
-                <span v-if="errorMsg=='passwords_not_same'" class="help is-danger">Passwörter stimmen nicht überein</span> 
-                <span v-if="errorMsg=='passwordNotValid'" class="help is-danger">Das Passwort muss mindestens 8 Zeichen lang sein</span>
-            </p>
-        </div>
+                <div class="field">
+                    <p class="control has-icons-left has-icons-right">
+                        <input required v-model.lazy="user.lastname"  id="login_lastname" name="lastname" class="input" type="text" placeholder="Last name">
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-user"></i>
+                        </span>
+                        <span v-if="errorMsg=='lastname'" class="help is-danger">Please enter your last name</span>      
+                    </p>
+                </div>
 
-        <div class="field">
-            <p class="control has-icons-left">
-                <input required v-model.lazy="user.password_confirmation" id="password_confirmation" class="input" type="password" placeholder="Passwort Bestätigung">
-                <span class="icon is-small is-left">
-                  <i class="fa fa-lock"></i>
-                </span>
-                <span v-if="errorMsg=='password_confirmation'" class="help is-danger">Bitte bestätigen Sie Ihr Passwort</span> 
-                <span v-if="errorMsg=='passwords_not_same'" class="help is-danger">Passwörter stimmen nicht überein</span> 
-            </p>
-        </div>
+                <div class="field">
+                    <p class="control has-icons-left has-icons-right">
+                        <input required v-model.lazy="user.email" id="login_email" name="email" class="input" type="email" placeholder="E-Mail">
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-envelope"></i>
+                        </span>
+                        <span v-if="errorMsg=='email'" class="help is-danger">Please enter a valid e-mail address</span>    
+                        <span v-if="errorMsg=='emailTaken'" class="help is-danger">This e-mail address is already in use</span>
+                    </p>
+                </div>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <input required v-model.lazy="user.password" id="password" class="input" type="password" placeholder="Password">
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-lock"></i>
+                        </span>
+                        <span v-if="errorMsg=='password'" class="help is-danger">please enter a password</span> 
+                        <span v-if="errorMsg=='passwords_not_same'" class="help is-danger">Passwords do not match</span> 
+                        <span v-if="errorMsg=='passwordNotValid'" class="help is-danger">The password must be at least 8 characters long</span>
+                    </p>
+                </div>
 
-        <div class="field">
-            <p class="control">
-                <button class="button is-success is-fullwidth" v-on:click="register()" :disabled="disabled">
-                  Registrieren
-                </button>
-                <br/>
-               <span>
-                   Hast du schon ein Login? <router-link class="button is-light is-fullwidth" to="/auth/login?type=organisation">Login</router-link>
-               </span>
-            </p>
-        </div>
+                <div class="field">
+                    <p class="control has-icons-left">
+                        <input required v-model.lazy="user.password_confirmation" id="password_confirmation" class="input" type="password" placeholder="Confrim password">
+                        <span class="icon is-small is-left">
+                        <i class="fa fa-lock"></i>
+                        </span>
+                        <span v-if="errorMsg=='password_confirmation'" class="help is-danger">Please confirm your password</span> 
+                        <span v-if="errorMsg=='passwords_not_same'" class="help is-danger">Passwords do not match</span> 
+                    </p>
+                </div>
+
+              <button class="button is-block is-primary is-large is-fullwidth" v-on:click="register()" @keyup.enter="register" :disabled="disabled">Sign up</button>
+            </form>
+          </div>
+            <!-- <div class="form-group">
+                <span id="errorMsg" style="color:red; font-weight: 600">{{errorMsg}}<br/></span>
+            </div> -->
+          <p class="has-text-grey">
+            <router-link to="/auth/login">Already have an account? Sign in</router-link> &nbsp;·&nbsp;
+            <router-link to="/contactUs">Need Help?</router-link>
+          </p>
     </div>
+
 </div>
 </template>
 
