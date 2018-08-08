e25f8be550ffe0cf6ae8d87a789bdaa09f5168ab
tenderribs <dc3dude@gmail.com>
2018-08-08 11:10:36 +0200

    cleaned the noob code

--- src/components/pages/Team.vue
+++ src/components/pages/Team.vue
@@ -3,7 +3,7 @@
         <div class="container has-text-centered">
             <div class="columns is-vcentered">
                 <div v-for="teamMember in teamMembers" :key="teamMember.id"  class="column is-6 " v-on:click="showModal()">
-                    <div class="hoverbox">
+                    
                             <figure class="image">
                                 <img :src="teamMember.imagePath" alt="Placeholder image">
                             </figure>
@@ -34,33 +34,6 @@
                             </a>
                         </p>   
 
-                    </div>
-
-                    <div class="title is-2">
-                        {{teamMember.name}}
-                        
-                    </div>
-                    <div class="subtitle is-4">
-                        <!-- Rank : {{teamMember.rank}} -->
-                        
-                    </div>
-                    <div class="subtitle is-6 team-description">
-                        {{teamMember.description}}
-                    </div>
-                    <br>
-                    <p class="has-text-centered">
-                        <a class="button is-info is-outlined" :href="teamMember.githubProfileLink" target="blank">
-                            <i class="fa fa-github fa-2x"></i>
-                            <!-- <font-awesome-icon :icon="['far', 'spinner']" /> -->
-                        </a>
-                        &nbsp;
-                        <a class="button is-info is-outlined">
-                            Learn more
-                        </a>
-                    </p>                    
-                    <p class="has-text-centered">
-                 
-                    </p>
                     </div>
                 </div>
             </div>
